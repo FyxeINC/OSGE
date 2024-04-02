7bb318ba5bb49e0a48f10796c35f47b0ed836d0d
Trevor <64767648+FyxeINC@users.noreply.github.com>
2024-04-02 15:56:54 -0500

    - added project

---
+++ OSRL_Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleRenderer;
+using W = ConsoleHelperLibrary.Classes.WindowUtility;
+
+public class Program
+{
+	protected static Display m_Display;
+
+	static int Main(string[] args)
+	{
+		Console.Title = "Roomy";
+		W.SetConsoleWindowPosition(W.AnchorWindow.Fill);
+		
+		ConsoleRenderer.ConsoleCanvas Canvas = new ConsoleCanvas (false, false);
+		Canvas.CreateBorder();
+		Canvas.Render();
+		//m_Display = new Display ();
+		//m_Display.Render();
+		Console.ReadKey();
+			
+		return 0;
+	}
+}
+
